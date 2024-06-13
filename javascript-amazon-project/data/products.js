1d6d44a35eaf366e9ce8ea9f5a235c07b15fcb5d
Aman Kumar <ar95046@gmail.com>
2024-06-13 22:21:31 +0530

    Use inheritance and polymorphism to add size chart

--- javascript-amazon-project/data/products.js
+++ javascript-amazon-project/data/products.js
@@ -33,19 +33,42 @@ class Product {
   getPrice() {
     return `$${formatCurrency(this.priceCents)}`;
   }
+
+  extraInfoHTML() {
+    return "";
+  }
 }
 
-const product1 = new Product({
-  id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-  rating: {
-    stars: 4.5,
-    count: 87,
-  },
-  priceCents: 1090,
-  keywords: ["socks", "sports", "apparel"],
-});
+//inheritance;
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails) {
+    //super() will call the parent constructor
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    //super.extraInfoHTML();
+    return `
+    <a href="${this.sizeChartLink}" target="_blank">
+    Size chart
+    </a>`;
+  }
+}
+
+// const product1 = new Product({
+//   id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+//   image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+//   name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+//   rating: {
+//     stars: 4.5,
+//     count: 87,
+//   },
+//   priceCents: 1090,
+//   keywords: ["socks", "sports", "apparel"],
+// });
 
 export const products = [
   {
@@ -519,5 +542,8 @@ export const products = [
     keywords: ["sweaters", "hoodies", "apparel", "mens"],
   },
 ].map((productDetails) => {
+  if (productDetails.type === "clothing") {
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
