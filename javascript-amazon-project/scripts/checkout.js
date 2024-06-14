8b189859c4aa56566e9f147f668d05fcca9a6f03
Aman Kumar <ar95046@gmail.com>
2024-06-14 16:09:43 +0530

    Learn promises and convert code to promises

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,10 +1,60 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 //import "../data/cart-class.js";
 //import "../data/backend-practice.js";
 
-loadProducts(() => {
+//Promise.all() helps us to run multiple Promises at the same time and it makes more efficient.
+Promise.all([
+  new Promise((resolve) => {
+    console.log("start promise");
+    loadProducts(() => {
+      console.log("finished loading");
+      resolve("value1");
+    });
+  }),
+
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve("value2");
+    });
+  }),
+]).then((value) => {
+  console.log("next step");
+  console.log(value);
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+/*
+new Promise((resolve) => {
+  console.log("start promise");
+  loadProducts(() => {
+    console.log("finished loading");
+    resolve();
+  });
+})
+  .then(() => {
+    return new Promise((resolve) => {
+      console.log("next step");
+      loadCart(() => {
+        resolve();
+      });
+    });
+  })
+  .then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+
+  */
+
+/*
+loadProducts(() => {
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+});
+*/
